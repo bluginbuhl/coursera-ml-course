65c201e8e62832c912b4a4d29c35230138003ebe
Benjamin R. Luginbuhl <ben.luginbuhl@gmail.com>
2020-12-24 15:27:51 -0800

    Working solution for price calculation

--- week2/assignment1/ex1/ex1_multi.m
+++ week2/assignment1/ex1/ex1_multi.m
@@ -105,8 +105,8 @@ fprintf('\n');
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
 
-price = theta(1) + theta(2)*1650 + theta(3)*3; % You should change this
-
+% price = theta(1) + theta(2)*(1650-mu(1)/sigma(1)) + theta(3)*(3-mu(2)/sigma(2));
+price = [1 (1650 - mu(1))/sigma(1) (3 - mu(2))/sigma(2)]*theta;
 
 % ============================================================
 
@@ -150,7 +150,7 @@ fprintf('\n');
 
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
-price = 0; % You should change this
+price = [1 1650 3]*theta; % You should change this
 
 
 % ============================================================
