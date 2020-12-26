04013810a18fe683f10329080d2702de971283be
Benjamin R. Luginbuhl <ben.luginbuhl@gmail.com>
2020-12-25 16:58:31 -0800

    Complete ex2_reg assignment

--- week3/assignment2/ex2/costFunctionReg.m
+++ week3/assignment2/ex2/costFunctionReg.m
@@ -21,9 +21,9 @@ h = sigmoid(X*theta);
 shift_theta = theta(2:size(theta));
 theta_reg = [0;shift_theta];
 
-J = -(1/m)*(y'*log(h) + (1 - y)'*log(1 - h)) + (lambda/2*m)*theta_reg'*theta_reg;
+J = -(1/m)*(y'*log(h) + (1 - y)'*log(1 - h)) + (lambda/(2*m))*theta_reg'*theta_reg;
 
-grad = (1/m)*X'*(h - y) + lambda*theta_reg;
+grad = (1/m)*(X'*(h - y) + lambda*theta_reg);
 
 % =============================================================
 
